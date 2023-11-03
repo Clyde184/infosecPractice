30df5c021f1abebb41b3eb6b436d22eb95b70b04
Clyde184 <audacity184@gmail.com>
2023-11-03 20:01:26 +0800

    helmet.dnsPrefetchControl()_fsttry

--- myApp.js
+++ myApp.js
@@ -17,11 +17,11 @@ app.use(helmet.xssFilter());
 app.use(helmet.noSniff());
 app.use(helmet.ieNoOpen());
 
-ninetyDaysInSeconds = 90*24*60*60;
+const ninetyDaysInSeconds = 90*24*60*60;
 
 app.use(helmet.hsts({maxAge: ninetyDaysInSeconds,force: true}));
 
-
+app.use(helmet.dnsPrefetchControl());
 
 
 
